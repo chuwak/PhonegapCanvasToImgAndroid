74cde30b7f342f73bcf6e6fe7f534339f08cb50a
chuwak <chuwak@ukr.net>
2014-07-03 19:58:28 +0300

    1.6

--- www/canvas_plugin.js
+++ www/canvas_plugin.js
@@ -1,20 +1,35 @@
-window.canvasplugin = function(canvasEl,callback){
-	var canvasProps = {
-		mimeType: "image/png", //this is hard coded right now, change it as per your need
-		xpos: canvasEl.offsetLeft,
-		ypos: canvasEl.offsetTop,
-		width: canvasEl.width,
-		height: canvasEl.height,
-		screenWidth: window.innerWidth // no WebView.getContentWidth(), use this instead
-	};
-	
-	//call the Plugin execute method()
-	cordova.exec(callback,function(err){
-		callback('Error: ' + err);	
-	},"CanvasPlugin","toDataURL",[canvasProps.mimeType,
-							canvasProps.xpos,
-							canvasProps.ypos,
-							canvasProps.width,
-							canvasProps.height,
-							canvasProps.screenWidth]);	
-}
+window.canvasplugin = function(canvasEl, offset, type, callback) {
+    var mineType = (type ? type : "image/png");
+    var xpos = canvasEl.offsetLeft;
+    var ypos = canvasEl.offsetTop;
+    var width = canvasEl.width;
+    var height = canvasEl.height;
+    var screenWidth = window.innerWidth; // no WebView.getContentWidth(), use this instead
+
+    if (typeof (offset) !== 'undefined' && offset) {
+        xpos = offset.left;
+        ypos = offset.top;
+        width = offset.width;
+        height = offset.height;
+        screenWidth = offset.screenWidth ? offset.screenWidth : screenWidth;
+    }
+    ;
+    var canvasProps = {
+        mimeType: mineType,
+        xpos: xpos,
+        ypos: ypos,
+        width: width,
+        height: height,
+        screenWidth: screenWidth
+    };
+
+    //call the Plugin execute method()
+    cordova.exec(callback, function(err) {
+        callback('Error: ' + err);
+    }, "CanvasToDataURL", "toDataURL", [canvasProps.mimeType,
+        canvasProps.xpos,
+        canvasProps.ypos,
+        canvasProps.width,
+        canvasProps.height,
+        canvasProps.screenWidth]);
+};
